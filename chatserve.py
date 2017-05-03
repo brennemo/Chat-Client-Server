0554d3bb3d7540c968eedea82598fb6ba8b273d8
Morgan Brenner <morganmbrenner@gmail.com>
2017-05-03 16:26:14 -0400

    placeholders for functions

--- chatserve.py
+++ chatserve.py
@@ -3,24 +3,15 @@
 # Echo server program
 import socket
 import sys
-import SocketServer
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+def start_up:
+	print "start up!"
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+def send_message:
+	print "sent message!"
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "%s wrote:" % self.client_address[0]
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.send(self.data.upper())
+def receive_message:
+	print "receive message!"
 
 if __name__ == "__main__":
     #Get port number 
@@ -28,34 +19,26 @@ if __name__ == "__main__":
         print "USAGE: ./chatserve <port #>"
         exit(1)
 
-    port_num = sys.argv[1]
+    port_num = int(sys.argv[1])
 
     #hard code handle
     handle_a = "blorps"
 
     HOST = ''               # Symbolic name meaning all available interfaces
     PORT = port_num            
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
-	s.bind((HOST, PORT))
-	s.listen(1)
-	
-	conn, addr = s.accept()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	print 'Connected by', addr
-	while 1:
-		data = conn.recv(1024)
-		if not data: break
-		conn.sendall(data)
-	conn.close()
+    s.bind((HOST, PORT))
+    s.listen(1)
 
+    conn, addr = s.accept()
 
-    #HOST, PORT = "localhost", port_num
+    print 'Connected by', addr
 
-    # Create the server, binding to localhost on port 9999
-    #server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    while 1:
+    	data = conn.recv(1024)
+    	if not data: break
+    	conn.sendall(data)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    #server.serve_forever()
+    conn.close()
 
